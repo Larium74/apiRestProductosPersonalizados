7208760d1eaea9fe9150c1d72f0cc698a85bc237
yeizer74 <yeimer.z@outlook.com>
2024-06-21 19:17:39 -0500

    Agregando Middlewheres

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ import express from 'express';
 import connectiondb from './database/connection.js'; // Ajusta la ruta según tu estructura de carpetas
 import clientesRouter from './routes/clientes.routes.js';
 import { PORT } from './config.js';
+import cors from "cors"
+
+
 const app = express();
+
+
+app.use (express.json ())
+app.use (express.urlencoded ({extended: false}))
+app.use (cors ())
 
 app.use ("/", clientesRouter)
 app.get('/', (req, res) => {
